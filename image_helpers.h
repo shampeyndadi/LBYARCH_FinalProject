ec9a4abc880420d6518cdb8d9ae47208b601f5c5
shampeyndadi <charlesxxavier30@gmail.com>
2025-11-24 02:40:29 +0800

    feat: header files

---
+++ image_helpers.h
@@ -0,0 +1,7 @@
+#ifndef IMAGE_HELPERS_H
+#define IMAGE_HELPERS_H
+
+void generateRandomImage(const char* filename, int width, int height);
+void printImageInt(int *arr, int h, int w);
+
+#endif
---
+++ imgCvtGrayFloatToInt.h
@@ -0,0 +1,6 @@
+#ifndef IMG_CVT_GRAY_FLOAT_TO_INT_H
+#define IMG_CVT_GRAY_FLOAT_TO_INT_H
+
+void imgCvtGrayFloatToInt(float *input, int *output, long totalPixels);
+
+#endif
